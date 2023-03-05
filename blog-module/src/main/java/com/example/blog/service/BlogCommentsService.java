c0c03419f2e4d13daa33716c46b60ca092d9377e
wei <422953951@qq.com>
2023-03-05 15:44:28 +0800

    私聊功能开发

--- blog-module/src/main/java/com/example/blog/service/BlogCommentsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.blog.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.blog.entity.BlogComments;
-
-public interface BlogCommentsService extends IService<BlogComments> {
-
-}
--- blog-module/src/main/java/com/example/blog/utils/SystemConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.utils;
-
-public class SystemConstants {
-    public static final String USER_NICK_NAME_PREFIX = "user_";
-    public static final int My_PAGE_SIZE = 5;
-    public static final int MAX_PAGE_SIZE = 10;
-}
